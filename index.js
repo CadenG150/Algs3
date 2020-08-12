99039ac6ae51d81dd5abeb0982059628689926f0
CadenG150 <cadengrey@icloud.com>
2020-08-12 14:24:12 -0400

    Basic SQL Stuff

--- index.js
+++ index.js
@@ -2,17 +2,18 @@ const express = require("express");
 const app = express();
 var path = require("path");
 const bodyParser = require("body-parser");
-var cookieParser = require("cookie-parser");
 const session = require("express-session");
 const Cubid = require('cubid');
 
-/*var mysql = require("mysql");
+const PLLNames = ["Aa", "Ab", "E", "F", "Ga", "Gb", "Gc", "Gd", "H", "Ja", "Jb", "Na", "Nb", "Ra", "Rb", "T", "Ua", "Ub", "V", "Y", "Z"];
+
+var mysql = require("mysql");
 var con = mysql.createConnection({
   host: "localhost",
   user: "caden",
-  password: "codepassword05",
-  database: "eaglecare"
-});*/
+  password: "Passwordfordatabase",
+  database: "algs3"
+});
 
 const yPerm1 = "F R U' R' U' R U R' F' R U R' U' R' F R F'"
 const yPerm2 = "F R' F R2 U' R' U' R U R' F' R U R' U' F'"
@@ -22,20 +23,13 @@ const newCube = cube.apply(yPerm2);
 
 console.log(newCube.isSolved("all"));
 
-app.use(
-  session({
-    secret: "secretsession",
-    resave: false,
-    saveUninitialized: true
-  })
-);
+app.locals.testVar = "x R' U R' D2 R U' R' D2 R2";
 
 app.set("view engine", "ejs");
 app.set("views", __dirname + "/views");
 app.use(express.static(__dirname + "/public"));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
-app.use(cookieParser());
 
 app.get("/", function(req, res) {
   res.render("index");
@@ -46,7 +40,14 @@ app.get("/3x3", function(req, res) {
 });
 
 app.get("/PLL", function(req, res) {
-  res.render("PLL");
+  var sql = "SELECT algorithm FROM algorithms WHERE AlgorithmName = " + con.escape(PLLNames[0]);
+  con.query(sql, function (err, result) {
+    if (err) throw err;
+    res.render("PLL", {
+      algorithmTest: result,
+    });
+    console.log("Result: " + result);
+  });
 });
 
 var http = require("http");
